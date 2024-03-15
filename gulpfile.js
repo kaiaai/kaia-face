92da8e72b44e192cf3ad3184e125b4555e6ee763
Ilia O <iliao@kaia.ai>
2024-03-15 04:12:01 +0000

    All seems to work

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ const imagemin_ = require('gulp-imagemin');
 const inject = require('gulp-js-base64-inject');
 const replace = require('gulp-replace');
 const umd_ = require('gulp-umd');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+//const changed_ = require('gulp-changed');
+const watch_ = require('gulp-watch');
 
 const LIB_NAME = 'kaia-face.js';
 const UMD_EXPORTS = 'Face';
@@ -22,38 +26,52 @@ const DIST_DIR = 'dist/';
 const BUNDLE_DIR = 'bundle/';
 const BASE64_DIR = 'base64/';
 
-function imagemin(cb) {
+function imagemin() {
   return src(SRC_DIR + IMG_DIR + '**/*')
     .pipe(imagemin_())
     .pipe(dest(BUNDLE_DIR + IMG_DIR));
 }
 
-function clean(cb) {
+function clean() {
   return src([DIST_DIR, BUNDLE_DIR, BASE64_DIR], {read: false, allowEmpty: true})
     .pipe(clean_());
 }
 
-function concat(cb) {
+function concat() {
   return src(SRC_JS)
     .pipe(concat_(LIB_NAME))
     .pipe(dest(BUNDLE_DIR));
 }
 
-function jshint_src(cb) {
+function jshint_src() {
   return src(SRC_JS)
     //.pipe(plumber())
     .pipe(jshint({ esversion: 6 }))
-    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('default'));
 }
 
-function jshint_bundle(cb) {
+function jshint_bundle() {
   return src(BUNDLE_DIR + LIB_NAME)
     //.pipe(plumber())
     .pipe(jshint({ esversion: 6 }))
     .pipe(jshint.reporter('default'));
 }
 
-function base64(cb) {
+function jshint_base64() {
+  return src(BASE64_DIR + LIB_NAME)
+    //.pipe(plumber())
+    .pipe(jshint({ esversion: 6 }))
+    .pipe(jshint.reporter('default'));
+}
+
+function jshint_umd() {
+  return src(DIST_DIR + LIB_NAME)
+    //.pipe(plumber())
+    .pipe(jshint({ esversion: 6 }))
+    .pipe(jshint.reporter('default'));
+}
+
+function base64() {
   return src(BUNDLE_DIR + LIB_NAME)
     .pipe(inject({
       basepath: BUNDLE_DIR + IMG_DIR,
@@ -64,7 +82,7 @@ function base64(cb) {
     .pipe(dest(BASE64_DIR));
 }
 
-function umd(cb) {
+function umd() {
   return src(BASE64_DIR + LIB_NAME)
     //.pipe(plumber())
     .pipe(umd_({
@@ -74,10 +92,61 @@ function umd(cb) {
     .pipe(dest(DIST_DIR));
 }
 
+function umd_step() {
+  return src(BASE64_DIR + LIB_NAME)
+    //.pipe(plumber())
+    .pipe(umd({
+      namespace: () => UMD_NAMESPACE,
+      exports: () => UMD_EXPORTS,
+    }))
+    .pipe(dest(DIST_DIR));
+}
+
+function minify() {
+  return src(DIST_DIR + LIB_NAME)
+    .pipe(uglify())
+    .pipe(rename((path) => {
+      path.basename += '.min';
+    }))
+    .pipe(dest(DIST_DIR));
+}
+
+function minify_step() {
+  return src(DIST_DIR + LIB_NAME)
+    .pipe(uglify())
+    .pipe(rename((path) => {
+      path.basename += '.min';
+    }))
+    .pipe(dest(DIST_DIR));
+}
+
+//"gulp-changed": "^4.0.3",
+//function changed() {
+//  return src(SRC_JS)
+//    .pipe(changed_(DIST_DIR))
+//    .pipe(dest(DIST_DIR));
+//}
+
 exports.clean = clean;
 exports.concat = series(exports.clean, concat);
 exports.jshint_src = jshint_src;
 exports.jshint_bundle = series(exports.concat, jshint_bundle);
 exports.imagemin = series(exports.concat, imagemin);
 exports.base64 = series(exports.imagemin, base64);
+exports.jshint_base64 = series(exports.base64, jshint_base64);
 exports.umd = series(exports.base64, umd);
+exports.jshint_umd = series(exports.umd, jshint_umd);
+exports.umd_step = umd_step;
+exports.minify = series(exports.umd, minify);
+exports.minify_step = minify_step;
+//exports.changed = changed;
+exports.default = exports.minify;
+
+function watch() {
+  const watcher = watch_(SRC_JS_DIR+'/*.js');
+  watcher.on('change', function(path, stats) {
+    exports.minify();
+  });
+}
+
+exports.watch = watch;
