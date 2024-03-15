8016c95fb5b8379761fb9869b33eb928f38f6aec
Ilia O <iliao@kaia.ai>
2024-03-15 02:11:12 +0000

    gulp base64

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const clean_ = require('gulp-clean');
 const concat_ = require('gulp-concat');
 const jshint = require('gulp-jshint');
 const imagemin_ = require('gulp-imagemin');
+const inject = require('gulp-js-base64-inject');
+const replace = require('gulp-replace');
 
 const LIB_NAME = 'kaia-face.js';
 const SRC_DIR = 'src/';
@@ -16,36 +18,7 @@ const DIST_DIR = 'dist/';
 const BUNDLE_DIR = 'bundle/';
 const BASE64_DIR = 'base64/';
 
-function copy(cb) {
-  src('src/js/*.js')
-    .pipe(dest('copies'));
-  cb();
-}
-
-function clean(cb) {
-  src([DIST_DIR, BUNDLE_DIR, BASE64_DIR], {read: false, allowEmpty: true})
-    .pipe(clean_());
-//  .pipe(wait(WAIT))
-  cb();
-}
-
-function concat(cb) {
-  src(SRC_JS)
-    .pipe(concat_(LIB_NAME))
-    .pipe(dest(BUNDLE_DIR));
-//    .pipe(wait(WAIT))
-  cb();
-}
-
-function jshint_bundle(cb) {
-  src(BUNDLE_DIR + LIB_NAME)
-    //.pipe(plumber())
-    .pipe(jshint({ esversion: 6 }))
-    .pipe(jshint.reporter('default'));
-  cb();
-}
-
-function imagemin(cb) {
+function imagemin_task(cb) {
   src(SRC_DIR + IMG_DIR + '**/*')
     .pipe(imagemin_())
     .pipe(dest(BUNDLE_DIR + IMG_DIR));
@@ -53,8 +26,44 @@ function imagemin(cb) {
   cb();
 }
 
-exports.copy = copy;
-exports.clean = clean;
-exports.concat = series(clean, concat);
-exports.jshint_bundle = series(concat, jshint_bundle);
-exports.imagemin = series(concat, imagemin);
+function clean_task(cb) {
+  src([DIST_DIR, BUNDLE_DIR, BASE64_DIR], {read: false, allowEmpty: true})
+    .pipe(clean_());
+//  .pipe(wait(WAIT))
+  cb();
+}
+
+function concat_task(cb) {
+  src(SRC_JS)
+    .pipe(concat_(LIB_NAME))
+    .pipe(dest(BUNDLE_DIR));
+//    .pipe(wait(WAIT))
+  cb();
+}
+
+function jshint_bundle_task(cb) {
+  src(BUNDLE_DIR + LIB_NAME)
+    //.pipe(plumber())
+    .pipe(jshint({ esversion: 6 }))
+    .pipe(jshint.reporter('default'));
+  cb();
+}
+
+function base64_task(cb) {
+  src(BUNDLE_DIR + LIB_NAME)
+    .pipe(inject({
+      basepath: BUNDLE_DIR + 'img/',
+      pattern: /['"]img\/([a-zA-Z0-9\-_.\\/]+)['"]/g
+      //debug: true
+    }))
+    .pipe(replace('image/png;base64', 'data:image/png;base64')) // inject bug workaround
+    .pipe(dest(BASE64_DIR));
+//  .pipe(wait(WAIT))
+  cb();
+}
+
+exports.clean = clean_task;
+exports.concat = series(clean_task, concat_task);
+exports.jshint_bundle = series(concat_task, jshint_bundle_task);
+exports.imagemin = series(concat_task, imagemin_task);
+exports.base64 = series(imagemin_task, base64_task);
