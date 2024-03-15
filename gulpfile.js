b26016e2b37fd4fdc5b6dcb4bc23b7afc8b018c1
Ilia O <iliao@kaia.ai>
2024-03-15 00:24:57 +0000

    gulp concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const { src, dest } = require("gulp");
+const { src, dest, series } = require("gulp");
 const clean_ = require('gulp-clean');
+const concat_ = require('gulp-concat');
 
+const LIB_NAME = 'kaia-face.js';
+const SRC_DIR = 'src/';
+const SRC_JS_DIR = SRC_DIR + 'js/';
+const SRC_JS = [SRC_JS_DIR + 'helpers.js', SRC_JS_DIR + 'animationModule.js',
+  SRC_JS_DIR + 'animationEffectModule.js', SRC_JS_DIR + 'main.js',
+  SRC_JS_DIR + 'user_api.js', SRC_JS_DIR + 'expression.js',
+  SRC_JS_DIR + 'expressionElement.js'];
+const IMG_DIR = 'img/';
 const DIST_DIR = 'dist/';
 const BUNDLE_DIR = 'bundle/';
 const BASE64_DIR = 'base64/';
@@ -18,5 +27,14 @@ function clean(cb) {
   cb();
 }
 
+function concat(cb) {
+  src(SRC_JS)
+    .pipe(concat_(LIB_NAME))
+    .pipe(dest(BUNDLE_DIR));
+//    .pipe(wait(WAIT))
+  cb();
+}
+
 exports.copy = copy;
 exports.clean = clean;
+exports.concat = series(clean, concat);
